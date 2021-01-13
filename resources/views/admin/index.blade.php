dda69062bf7bea8275f1be362ff4249c573789e5
aboutmf <mf.about@gmail.com>
2021-01-13 10:51:58 +0700

    Tambah CRUD

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <h1>Tambah CRUD</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
